ded6d3fd2e77cd0b717bf15904d949bc9f35eeba
thePhantom2 <135716593+thePhantom2@users.noreply.github.com>
2024-03-17 10:46:36 +0100

    Add files via upload

---
+++ komendy.sh
@@ -0,0 +1,89 @@
+git commit
+git commit
+
+
+git checkout -b bugfix
+
+
+git checkout -b bugfix
+git commit
+git checkout main
+git commit
+git merge bugfix
+
+
+git checkout -b bugfix
+git commit
+git checkout main
+git commit
+git checkout bugfix
+git rebase main
+
+
+git checkout C4
+
+git checkout C3
+
+
+git branch -f main c6
+git checkout HEAD~1
+git branch -f bugFix HEAD~1
+
+
+
+git reset main
+git checkout pushed
+git revert pushed
+ 
+
+git cherry-pick c3 c4 c7
+
+
+git rebase -i main~4
+
+
+git rebase -i bugFix~3
+git checkout main
+git rebase bugFix
+
+
+git rebase -i HEAD~2
+git commit --amend
+git rebase -i HEAD~2
+git checkout main
+git rebase caption
+
+
+git cherry-pick c2
+git checkout main
+git commit --amend
+git cherry-pick c3
+
+
+git tag v0 c1
+git tag v1 c2
+git checkout c2
+
+
+git commit
+
+
+git checkout bugFix
+git rebase main
+git rebase c3' c4
+git checkout side
+git rebase c4'
+git checkout another 
+git rebase c6'
+git checkout main
+git rebase another
+
+
+git checkout HEAD~^2~;git branch bugWork;git checkout main
+
+git rebase -i c1 c5
+git checkout one
+git rebase c2'
+git checkout three;git rebase c2
+git rebase - i c1 c5
+git checkout two;git rebase c2''
